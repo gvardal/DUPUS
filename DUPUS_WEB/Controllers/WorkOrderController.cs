c0bccfb236bec76cc267ae7fd113a70a1c7ec650
Giray VARDAL <gvardal@yahoo.com>
2023-06-14 08:54:30 +0300

    Create WEB Project

---
+++ DUPUS_WEB/Controllers/WorkOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DUPUS_WEB.Controllers
+{
+    public class WorkOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
